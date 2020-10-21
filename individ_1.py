5cf9c0c06e8a9ccc89e642ba72e9f4a0bb1d14bb
karrrakotov <karrrakotov@mail.ru>
2020-10-21 14:29:19 +0300

    Сделал лабораторную

---
+++ individ_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    a = input("Введите текст: ")
+
+    for counter in range(len(a)):
+        print(a[counter], end='\n')
