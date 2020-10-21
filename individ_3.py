deb9cfc365b8cee56910af9403bade30316bd69f
karrrakotov <karrrakotov@mail.ru>
2020-10-21 17:15:43 +0300

    Дополнил файл individ_3.py

--- individ_3.py
+++ individ_3.py
@@ -3,6 +3,6 @@
 
 if __name__ == '__main__':
     s = input("Введите предложение: ")
-    bad_chars = ['с', 'c']
+    bad_chars = ['с', 'c', 'С', 'C']
     s = ''.join(i for i in s if not i in bad_chars)
     print("Предложение после замены: " + s)
